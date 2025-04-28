e7fc21954e6e8a308fc19526716b213f0ece9269
Ali Gürkan Sevilmiş <81529791+GurkanSvlms@users.noreply.github.com>
2025-04-28 12:00:56 +0300

    Login Page Added

---
+++ ISKI.SARS.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ISKI.SARS.WebUI.Models;
+
+namespace ISKI.SARS.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Burada login işlemini yaparsın
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ ISKI.SARS.WebUI/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ISKI.SARS.WebUI.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "E-Posta giriniz")]
+        [EmailAddress(ErrorMessage = "Geçerli bir e-posta adresi giriniz")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Şifre giriniz")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
