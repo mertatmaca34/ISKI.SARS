e59ea6ea7f1259e1bce578eab9a278f44fa6e845
Mert Atmaca <69796541+mertatmaca34@users.noreply.github.com>
2025-07-08 11:26:16 +0300

    Add OperationClaims Razor pages and view model

---
+++ ISKI.SARS.WebUI/Pages/OperationClaims/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using AutoMapper;
+using ISKI.SARS.Application.Features.OperationClaims.Dtos;
+using ISKI.SARS.Application.Features.OperationClaims.Queries.GetList;
+using ISKI.SARS.WebUI.Services;
+using ISKI.SARS.WebUI.ViewModels.OperationClaims;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ISKI.SARS.WebUI.Pages.OperationClaims;
+
+public class IndexModel(ApiService apiService, IMapper mapper) : PageModel
+{
+    private readonly ApiService _apiService = apiService;
+    private readonly IMapper _mapper = mapper;
+
+    public List<OperationClaimVm> Claims { get; set; } = new();
+
+    public async Task OnGetAsync()
+    {
+        var result = await _apiService.GetAsync<PaginatedList<OperationClaimDto>>("api/OperationClaims");
+        if (result != null)
+            Claims = _mapper.Map<List<OperationClaimVm>>(result.Items);
+    }
+
+    public class PaginatedList<T>
+    {
+        public List<T> Items { get; set; } = new();
+    }
+}
