03ecafa9f7d7f8aded9c51812a64eaa366a0a00d
Mert Atmaca <69796541+mertatmaca34@users.noreply.github.com>
2025-07-17 16:55:12 +0300

    Add OPC UA worker

--- ISKI.SARS.Worker/Program.cs
+++ ISKI.SARS.Worker/Program.cs
@@ -1,6 +1,13 @@
 using ISKI.SARS.Worker;
+using ISKI.SARS.Application;
+using ISKI.SARS.Infrastructure;
+using ISKI.OpcUa.Client;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddIskiOpcUaClient();
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration.GetConnectionString("DefaultConnection")!);
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
