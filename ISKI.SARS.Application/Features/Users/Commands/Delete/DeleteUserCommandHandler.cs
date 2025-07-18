d1fda3aae2589f77b664d8a0793248ce2dc626ee
Mert Atmaca <69796541+mertatmaca34@users.noreply.github.com>
2025-07-18 09:24:01 +0300

    Delete user's claims when deleting user

--- ISKI.SARS.Application/Features/Users/Commands/Delete/DeleteUserCommandHandler.cs
+++ ISKI.SARS.Application/Features/Users/Commands/Delete/DeleteUserCommandHandler.cs
@@ -1,18 +1,27 @@
 ﻿using AutoMapper;
-using ISKI.Core.Security.Entities;
 using ISKI.Core.Security.Repositories;
 using ISKI.SARS.Application.Features.Users.Rules;
 using MediatR;
 
 namespace ISKI.SARS.Application.Features.Users.Commands.Delete;
 
-public class DeleteUserCommandHandler(IUserRepository repository, IMapper mapper, UserBusinessRules rules) : IRequestHandler<DeleteUserCommand, DeletedUserResponse>
+public class DeleteUserCommandHandler(
+    IUserRepository repository,
+    IMapper mapper,
+    UserBusinessRules rules,
+    IUserOperationClaimRepository userOperationClaimRepository)
+    : IRequestHandler<DeleteUserCommand, DeletedUserResponse>
 {
     public async Task<DeletedUserResponse> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
     {
         await rules.UserMustExist(request.Id);
 
         var user = await repository.GetByIdAsync(request.Id);
+
+        var userClaims = await userOperationClaimRepository.GetAllAsync(x => x.UserId == request.Id);
+        foreach (var claim in userClaims)
+            await userOperationClaimRepository.DeleteAsync(claim);
+
         var deleted = await repository.DeleteAsync(user);
 
         return mapper.Map<DeletedUserResponse>(deleted);
