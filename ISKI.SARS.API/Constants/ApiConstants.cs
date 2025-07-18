227abd9d09396dd808b23ff52150d65466b5f8e0
Mert Atmaca <69796541+mertatmaca34@users.noreply.github.com>
2025-07-18 10:43:18 +0300

    refactor: centralize API and WebUI string constants

---
+++ ISKI.SARS.API/Constants/ApiConstants.cs
@@ -0,0 +1,15 @@
+namespace ISKI.SARS.API.Constants
+{
+    public static class ApiConstants
+    {
+        public const string TokenOptionsSection = "TokenOptions";
+        public const string DefaultConnection = "DefaultConnection";
+        public const string AllowAllCorsPolicy = "AllowAll";
+        public const string SwaggerTitle = "ISKI.SARS API";
+        public const string SwaggerVersion = "v1";
+        public const string JwtScheme = "bearer";
+        public const string JwtBearerFormat = "JWT";
+        public const string AuthorizationHeader = "Authorization";
+        public const string BearerReference = "Bearer";
+    }
+}
---
+++ ISKI.SARS.WebUI/Constants/WebUiConstants.cs
@@ -0,0 +1,8 @@
+namespace ISKI.SARS.WebUI.Constants
+{
+    public static class WebUiConstants
+    {
+        public const string DefaultRouteName = "default";
+        public const string ErrorRoute = "/Home/Error";
+    }
+}
